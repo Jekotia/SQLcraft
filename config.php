a9f3eafa9838a3014313d5e0c48e7cf873d3592c
Jekotia <ningleepo@gmail.com>
2012-02-17 23:44:13 -0500

    House cleaning to prepare the project for revival.

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+	$mysql_host	=	'localhost';
+	$mysql_user	=	'root';
+	$mysql_pass	=	'derp';
+	$mysql_db	=	'sqlcraft';
+
+	$path_wr	=	'http://localhost/SQLcraft/';
+	$path_fs	=	'C:/Users/jekotia/My Dropbox/GitHub/SQLcraft/';
+	$path_mc	=	'C:/SQLcraft/';
+
+	$sc_auth	=	'1';
+	$sc_local	=	'0';
+	$sc_linux	=	'0';
+	$sc_screen	=	'mcserver';
+
+	$cn_user	=	'sqlcraft_user'
+	$cn_token	=	'sqlcraft_token';
+?>
